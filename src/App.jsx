8edf27b472b24c46410ca9746a7b16b7ca164bd7
MayaSker2 <mayasker8@gmail.com>
2024-05-04 18:49:39 +0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
-// import { Navbars } from './components/Navbar/Navbar'
-// import { Hero } from './sections/HeroSection/Hero'
-// import Skills from './sections/SkillsSection/Skills'
+import { Navbars } from './components/Navbar/Navbar'
+import { Hero } from './sections/HeroSection/Hero'
+import Skills from './sections/SkillsSection/Skills'
 import Services from "./sections/ServicesSection/Services";
 function App() {
   return (
     <>
-      {/* <Navbars/>
+      <Navbars/>
       <Hero />
-      <Skills/> */}
+      <Skills/>
       <Services />
     </>
   );
