daa0915323ed1db791c11a567ea75eb9a70a8e39
bodorha-cloud <bodorh1994@gmail.com>
2024-05-04 02:51:25 -0700

    About commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AboutMe from './sections/AboutMeSection/AboutMe'
+import Skills from './sections/SkillsSection/Skills'
+
+function App() {
+
+  return (
+    <>
+  <AboutMe/>
+    <Skills/>
+    
+    </>
+  )
+}
+
+export default App
