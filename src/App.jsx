a0625f342d6f106472be67e2b8a7089c98058325
alissar1998 <elissarabdeen@gmail.com>
2024-05-31 16:38:17 +0300

    design resume section and upload it with hire section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+// import Hire from "../src/sections/HireSection/Hire";
+import Resume from "./sections/ResumeSection/Resume"
 
 function App() {
-  return <>hello focal-x</>;
+  return (
+    <>
+      {/* hello focal-x */}
+      {/* <Hire></Hire> */}
+      <Resume></Resume>
+    </>
+  );
 }
 
 export default App;
--- src/sections/HireSection/Hire.jsx
+++ src/sections/HireSection/Hire.jsx
@@ -1,17 +1,19 @@
 import "./Hire.css";
+// import SectionHeader from "../../components/SectionHeader/SectionHeader"
 const Hire = () => {
   return (
     <section
       className="HireMe img"
       style={{ backgroundImage: `url(images/bg_1.jpg)` }}
     >
+      {/* <SectionHeader/> */}
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-md-7  text-center">
             <h2>
               I&#39;m <span>Available</span> for freelancing
             </h2>
-            <p style={{color:  `#999999 `}}>
+            <p style={{ color: `#999999 ` }}>
               A small river named Duden flows by their place and supplies it
               with the necessary regelialia.
             </p>
